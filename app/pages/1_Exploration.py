985bab863e4c03eb32bfe931530a7d14e632a2a7
Lukas Wiese <eseiwsakul@gmail.com>
2022-11-29 22:22:20 +0100

    Created some pie charts

--- app/pages/1_Exploration.py
+++ app/pages/1_Exploration.py
@@ -8,6 +8,7 @@ import yaml
 import plotly.express as px
 import plotly.graph_objects as go
 import plotly.figure_factory as ff
+from plotly.subplots import make_subplots
 
 
 # All rating columns
@@ -30,7 +31,7 @@ with open('../params.yaml') as file:
 
 st.set_page_config(
 	page_title='Exploration',
-	page_icon='👋')
+	page_icon='')
 
 
 st.title("Flight Ratings Exploration")
@@ -38,7 +39,25 @@ st.title("Flight Ratings Exploration")
 data = pd.read_csv(config['data']['explore'])
 
 
-# Plot average rating by flight class
+def plot_overall_rating_ditribution():
+	fig = px.histogram(data, x='rating_avg')
+	fig.update_layout(yaxis_title='', xaxis_title='Rating',
+			title='Overall Rating')
+	st.plotly_chart(fig, use_container_size=True)
+
+# Plot average rating by gender
+def plot_ratings_by_gender():
+
+	grp = data.pivot_table(index='gender', values='rating_avg').reset_index()
+
+	fig = px.bar(grp, x='gender', y='rating_avg', color='gender',
+		color_discrete_map={'M':'#6699ff', 'F':'#ffb3ff'})
+	fig.update_layout(xaxis_title='Gender',
+			yaxis_title='Average Rating',
+			title='Rating by Gender')
+	st.plotly_chart(fig, use_container_size=True)
+
+# Plot rating by flight class
 def plot_ratings_by_flight_class():
 
 	grp = data.pivot_table(index='class', values='rating_avg').reset_index()
@@ -49,6 +68,19 @@ def plot_ratings_by_flight_class():
 			yaxis_title='Average Rating',
 			title='Rating by flight class')
 	st.plotly_chart(fig, use_container_size=True)
+
+# Plot rating by travel type
+def plot_rating_by_travel_type():
+
+	grp = data.pivot_table(index='type_of_travel', values='rating_avg').reset_index()
+
+	fig = px.bar(grp, x='type_of_travel', y='rating_avg', color='type_of_travel',
+		color_discrete_map={'Business':'#b30059', 'Private':'#4d0026'})
+	fig.update_layout(xaxis_title='Type of Travel',
+			yaxis_title='Average Rating',
+			title='Rating by travel type')
+	st.plotly_chart(fig, use_container_size=True)
+
 
 
 # Plot average rating by age
@@ -94,15 +126,47 @@ def plot_best_worst_features():
 			title='Best/Worst features', height=600)
 	st.plotly_chart(fig, use_container_size=False)
 
-def plot_overall_rating_ditribution():
-	fig = px.histogram(data, x='rating_avg')
-	fig.update_layout(yaxis_title='', xaxis_title='Rating',
-			title='Overall Rating')
-	st.plotly_chart(fig, use_container_size=True)
 
 
+def plot_categorical_distribution():
+	fig = make_subplots(rows=3, cols=2, specs=[[{'type':'domain'}, {'type':'domain'}],
+                                           [{'type':'domain'}, {'type':'domain'}],
+                                           [{'type':'domain'}, {'type':'domain'}]])
+
+	labels = data['gender'].value_counts().index
+	values = data['gender'].value_counts().values
+	fig.add_trace(go.Pie(labels=labels, values=values, name="Gender", title='Gender'), 1, 1)
+
+	labels = data['class'].value_counts().index
+	values = data['class'].value_counts().values
+	fig.add_trace(go.Pie(labels=labels, values=values, name="Flight Class", title='Flight Class'), 1, 2)
+
+	labels = data['customer_type'].value_counts().index
+	values = data['customer_type'].value_counts().values
+	fig.add_trace(go.Pie(labels=labels, values=values, name="Customer Type", title='Customer Type'), 2, 1)
+
+	labels = data['flight_distance_class'].value_counts().index
+	values = data['flight_distance_class'].value_counts().values
+	fig.add_trace(go.Pie(labels=labels, values=values, name="Flight Distance", title='Flight Distance'), 2, 2)
+
+	labels = data['type_of_travel'].value_counts().index
+	values = data['type_of_travel'].value_counts().values
+	fig.add_trace(go.Pie(labels=labels, values=values, name="Travel Reason", title='Travel Reason'), 3, 1)
+
+	labels = data['generation'].value_counts().index
+	values = data['generation'].value_counts().values
+	fig.add_trace(go.Pie(labels=labels, values=values, name="Generation", title='Generation'), 3, 2)
+
+	fig.update_layout(title_text="Distribution of categoricals", height=2000)
+	st.plotly_chart(fig)
+
+
+# Apply plots
 plot_overall_rating_ditribution()
+plot_categorical_distribution()
 plot_best_worst_features()
+plot_ratings_by_gender()
 plot_ratings_by_flight_class()
 plot_rating_by_flight_distance()
+plot_rating_by_travel_type()
 plot_rating_by_age()
