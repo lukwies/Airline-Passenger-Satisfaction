a8f58e8038aab80d4ce48eb37a1793c5efc29673
Lukas Wiese <eseiwsakul@gmail.com>
2022-11-29 22:25:02 +0100

    Can predict satisfaction level now

--- app/pages/4_Survey.py
+++ app/pages/4_Survey.py
@@ -2,27 +2,19 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import time
-#
-# Run with:
-# $ streamlit run app.py
-#
+import yaml
+import pickle
 
-#st.write("Here's our first attempt at using data to create a table:")
+from sys import path
+from os.path import abspath
 
-
-#add_selectbox = st.sidebar.selectbox(
-#	"How would you like to be contacted?",
-#	("Email", "Home phone", "Mobile phone")
-#)
-#if st.sidebar.button('Push me'):
-#	st.sidebar.write('Hello World')
+path.insert(0, abspath('../src'))
+import mylib.transform as transform
 
 
 st.set_page_config(
 	page_title='Survey',
-	page_icon='👋'
-)
-
+	page_icon='👋')
 
 st.title("Flight Satisfaction Survey")
 
@@ -31,12 +23,12 @@ st.title("Flight Satisfaction Survey")
 gender = st.selectbox('Please select your gender', ('Female', 'Male'))
 customer_type = st.selectbox('Please select a customer type', ('Loyal', 'Disloyal'))
 age = st.number_input('Enter your age', min_value=6, max_value=120, value=35, step=1)
-travel_type = st.selectbox('Please select purpose of flight?', ('Private', 'Business'))
-flight_class = st.selectbox('Please select booked class?', ('Economy', 'Economy Plus', 'Business'))
+travel_type = st.selectbox('Please select purpose of flight', ('Private', 'Business'))
+flight_class = st.selectbox('Please select booked class', ('Eco', 'Eco Plus', 'Business'))
 flight_distance = st.number_input('Enter the flight distance', min_value=10, max_value=10000, value=1000, step=10)
 rate_wifi = st.slider('How would you rate the inflight wifi service?', 0,5)
 rate_time_convenient = st.slider('How convenient was the departure/arrival time?', 0, 5)
-rate_ease_booking = st.slider('How would you rate the ease of online booking?', 0, 5)
+rate_online_booking = st.slider('How would you rate the ease of online booking?', 0, 5)
 rate_gate_location = st.slider('How would you rate the gate location?', 0, 5)
 rate_food = st.slider('How would you rate food and drinking?', 0,5)
 rate_online_boarding = st.slider('How would you rate the online boarding?', 0, 5)
@@ -52,14 +44,59 @@ departure_delay = st.number_input('What was the departure delay (in minutes)?', 
 arrival_delay = st.number_input('What was the arrival delay (in minutes)?', min_value=0, max_value=10000, step=1)
 
 
+def predict_satisfaction():
+	d = {
+		'gender': ['M' if gender=='Male' else 'F'],
+		'customer_type': [customer_type],
+		'age': [age],
+		'type_of_travel': [travel_type],
+		'class': [flight_class],
+		'flight_distance': [flight_distance],
+		'inflight_wifi_service': [rate_wifi],
+		'departure_arrival_time_convenient': [rate_time_convenient],
+		'ease_of_online_booking': [rate_online_booking],
+		'gate_location': [rate_gate_location],
+		'food_and_drink': [rate_food],
+		'online_boarding': [rate_online_boarding],
+		'seat_comfort': [rate_seat],
+		'inflight_entertainment': [rate_entertainment],
+		'on_board_service': [rate_onboard_service],
+		'leg_room_service': [rate_legroom_service],
+		'baggage_handling': [rate_baggage_handling],
+		'checkin_service': [rate_checkin_service],
+		'inflight_service': [rate_inflight_service],
+		'cleanliness': [rate_cleanliness],
+		'departure_delay_in_minutes': [departure_delay],
+		'arrival_delay_in_minutes': [arrival_delay]
+	}
+	X = pd.DataFrame(d)
 
 
-#'You selected: ', rate_wifi
+	# Open yaml config
+	with open('../params.yaml') as file:
+		config = yaml.safe_load(file)
 
+	# Load random forest classifier
+	with open(config['model']['randForest'], 'rb') as file:
+		model = pickle.load(file)
+
+	X = transform.scale_and_encode_unseen(X)
+	y_pred = model.predict(X)
+#	y_pred_proba = model.predict_proba(X)
+
+#	proba = pd.DataFrame({'True': y_pred_proba[1],
+#				'False': y_pred_proba[0]})
+
+	if y_pred[0] == True:
+		st.markdown("### Luckily you are satisfied :-)")
+	else:
+		st.markdown("### Sorry for not being satisfied with our service :-(")
+
+#	st.write(type(y_pred_proba[0][y_pred[0]]))
+
+
+# Press submit button to predict if customer is satisfied
 if st.button('Submit'):
-	chart_data = pd.DataFrame(
-		np.random.randn(20, 3),
-		columns=['a', 'b', 'c'])
+	predict_satisfaction()
 
-	st.line_chart(chart_data)
-	st.write("...")
+
