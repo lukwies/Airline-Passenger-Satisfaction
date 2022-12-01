d7055bf1022ff85ed6dcebe7e1464b4b4ed5f82a
Lukas Wiese <eseiwsakul@gmail.com>
2022-12-01 15:56:32 +0100

    Adding labels for confusion matrix is possible now

--- src/mylib/evaluating.py
+++ src/mylib/evaluating.py
@@ -6,7 +6,7 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import precision_recall_curve
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
-def print_classification_results(model, y, y_pred):
+def print_classification_results(model, y, y_pred, labels=['True','False']):
 	'''
 	Show classification results.
 	This will create a plot with the confusion matrix and another one
@@ -21,7 +21,7 @@ def print_classification_results(model, y, y_pred):
 	fig,ax = plt.subplots(1,2, figsize=(9,3))
 
 	cm = confusion_matrix(y, y_pred)
-	ConfusionMatrixDisplay(cm, display_labels=['True','False']).plot(ax=ax[0])
+	ConfusionMatrixDisplay(cm, display_labels=labels).plot(ax=ax[0])
 
 	precisions, recalls, thresholds = precision_recall_curve(y, y_pred)
 	ax[1].plot(thresholds, precisions[:-1], "b--", label="Precision")
